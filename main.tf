1d305177bf834480350eca94c67b00331f22eaaf
ClémentPA <78507969+blbcrv@users.noreply.github.com>
2024-08-10 17:11:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,20 @@
 provider "aws" {
-  region = "eu-west-1"  # Région AWS où tu veux déployer l'instance
+  region = "eu-west-1"
 }
 
 # Créer ou importer une paire de clés SSH sur AWS
 resource "aws_key_pair" "key_pair" {
-  key_name   = "keyPairIPIDevOps"             # Nom de la clé SSH sur AWS
+  key_name   = "keyPairIPIDevOps"            
   public_key = file("~/.ssh/id_rsa_2048.pub")
 }
 
-# Définir un profil IAM pour autoriser l'instance EC2 à utiliser SSM
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ssm_profile"
-}
-
-
-# Créer une instance EC2 en utilisant la paire de clés SSH et le profil IAM SSM
 resource "aws_instance" "web_server" {
-  ami           = "ami-0c38b837cd80f13bb"    # AMI pour Ubuntu 22.04 LTS
+  ami           = "ami-0c38b837cd80f13bb"   
   instance_type = "t2.micro"
 
-  key_name               = aws_key_pair.key_pair.key_name  # Utilisation de la clé SSH
-  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name  # Associer le profil IAM pour SSM
+  key_name               = aws_key_pair.key_pair.key_name 
 
   tags = {
-    Name = "ProjetDevOpsWebServer"          # Nom de l'instance
+    Name = "ProjetDevOpsWebServer"          
   }
 }
-
-# Sortie de l'adresse IP publique de l'instance EC2
-output "instance_ip" {
-  value = aws_instance.web_server.public_ip
-}
-
